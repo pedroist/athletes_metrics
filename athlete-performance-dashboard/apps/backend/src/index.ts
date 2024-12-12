16050f2700bd0421602fba32164e9f98a2db9bcb
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2024-12-12 18:26:54 +0100

    lib/prisma.ts to call it in several places

--- athlete-performance-dashboard/apps/backend/src/index.ts
+++ athlete-performance-dashboard/apps/backend/src/index.ts
@@ -2,13 +2,12 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { cors } from 'hono/cors'
-import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
 import { athleteRoutes } from './routes/athlete.routes'
 import { errorHandler } from './middlewares/error.middleware'
+import { prisma } from './lib/prisma'
 
 const app = new Hono()
-export const prisma = new PrismaClient()
 
 // Global middleware
 app.use('*', logger())
--- athlete-performance-dashboard/apps/backend/src/routes/athlete.routes.ts
+++ athlete-performance-dashboard/apps/backend/src/routes/athlete.routes.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator'
-import { prisma } from '../index'
 import { AthleteRepository } from '../repositories/athlete.repository'
 import { AthleteService } from '../services/athlete.service'
 import { AthleteController } from '../controllers/athlete.controller'
 import { createAthleteSchema, createMetricSchema, updateAthleteSchema } from '../validators/athlete.validator'
+import { prisma } from '../lib/prisma'
 
 const athleteRoutes = new Hono()
 
@@ -14,14 +14,15 @@ const service = new AthleteService(repository)
 const controller = new AthleteController(service)
 
 // Routes
-athleteRoutes.get('/', controller.getAllAthletes)
-
-athleteRoutes.get('/:id', controller.getAthleteById)
 
 athleteRoutes.post('/', 
   zValidator('json', createAthleteSchema),
   controller.createAthlete
 )
+
+athleteRoutes.get('/', controller.getAllAthletes)
+
+athleteRoutes.get('/:id', controller.getAthleteById)
 
 athleteRoutes.put('/:id',
   zValidator('json', updateAthleteSchema),
