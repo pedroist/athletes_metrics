80f0b5bb4d2aeba1cc594da8a772448967fbbd3c
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2024-12-12 17:09:38 +0100

    updated routes

--- athlete-performance-dashboard/apps/backend/src/routes/athlete.routes.ts
+++ athlete-performance-dashboard/apps/backend/src/routes/athlete.routes.ts
@@ -1,11 +1,40 @@
 import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator'
+import { prisma } from '../index'
+import { AthleteRepository } from '../repositories/athlete.repository'
+import { AthleteService } from '../services/athlete.service'
+import { AthleteController } from '../controllers/athlete.controller'
+import { createAthleteSchema, createMetricSchema, updateAthleteSchema } from '../validators/athlete.validator'
 
 const athleteRoutes = new Hono()
 
-// Basic test route
-athleteRoutes.get('/', (c) => {
-  return c.json({ message: 'Athletes API is working' })
-})
+// Initialize dependencies - these are not static classes, they need an instance before being injected
+const repository = new AthleteRepository(prisma)
+const service = new AthleteService(repository)
+const controller = new AthleteController(service)
 
-export { athleteRoutes }
+// Routes
+athleteRoutes.get('/', controller.getAllAthletes)
+
+athleteRoutes.get('/:id', controller.getAthleteById)
+
+athleteRoutes.post('/', 
+  zValidator('json', createAthleteSchema),
+  controller.createAthlete
+)
+
+athleteRoutes.put('/:id',
+  zValidator('json', updateAthleteSchema),
+  controller.updateAthlete
+)
+
+athleteRoutes.delete('/:id', controller.deleteAthlete)
+
+athleteRoutes.post('/:id/metrics',
+  zValidator('json', createMetricSchema),
+  controller.createMetric
+)
+
+athleteRoutes.get('/:id/metrics', controller.getAthleteMetrics)
+
+export { athleteRoutes } 
